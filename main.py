90db185af38f716ca679643523473376d2855403
CubeVic <victoraguirre.f@gmail.com>
2022-12-11 21:16:53 +0800

    cleaning and fixing

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from metals.metal_prices import fetch
 from metals.models import Prices
 from metals import metals
 from dotenv import load_dotenv
+import initialize
 
 load_dotenv()
 
 if __name__ == "__main__":
-
+	metals.create_db_and_tables()
+	initialize.initialize_records()
 	record = metals.select_record(
 		table=Prices,
-		where_clause=Prices.metal_name == 'Silver')
+		where_clause=Prices.metal == 'XAG')
 	records = metals.select_records(
 		table=Prices,
-		where_clause=Prices.metal_name == 'Silver')
+		where_clause=Prices.metal == 'XAG')
 	print(f"record : {record}")
 	print(f"records : {records}")
 	print(fetch())
--- metals/models.py
+++ metals/models.py
@@ -5,8 +5,23 @@ from sqlmodel import SQLModel, Field
 
 class Prices(SQLModel, table=True):
 	id: Optional[int] = Field(default=None, primary_key=True)
-	time_stamp:  int
-	metal_symbol: str
-	metal_name: str
-	price: float
+	timestamp: int
+	metal: str
 	currency: str
+	exchange: str
+	symbol: str
+	prev_close_price: float
+	open_price: float
+	low_price: float
+	high_price: float
+	open_time: int
+	price: float
+	ch: float
+	chp: float
+	ask: float
+	bid: float
+	price_gram_24k: float
+	price_gram_22k: float
+	price_gram_21k: float
+	price_gram_20k: float
+	price_gram_18k: float
