e11614213f7802d00f98f136bc527a94d655da2e
Victor Fernandez <CubeVic@users.noreply.github.com>
2022-12-11 21:17:57 +0800

    cleaning and fixing (#9)

--- initialize.py
+++ initialize.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 """ initialize the database"""
 from metals import metals
-from metals.metal_prices import fetch_data
+from metals.metal_prices import fetch
 from metals.metals import insert_all
 from metals.models import Prices
 
 
 def initialize_records():
-	items = ["Silver", "Gold", "Platinum", "Palladium"]
+	items = ["silver", "gold", "platinum", "palladium"]
 	metals_info = []
 
 	for item in items:
-		_, resp_silver = fetch_data(metal=item)
-		metal = Prices(**resp_silver['metal_data'])
+		resp_silver = fetch(data=item)
+		metal = Prices(**resp_silver)
 		metals_info.append(metal)
 
 	insert_all(items=metals_info)
